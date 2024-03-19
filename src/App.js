f87ef7c7ce70427eba5fd2eed2bd4cbc39ceb4ab
Ariana <152944814+aam214@users.noreply.github.com>
2024-03-19 12:44:07 -0500

    Added react, imported bootstrap, imported dictionary logo, added Bootstrap class to logo.

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.png";
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <header>
+        <img src={logo} width="200"className="rounded img-thumbnail" alt="logo"/>
+      </header>
+    </div>
+    
+  );
+}
