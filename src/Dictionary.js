23afb920ad1d89d8197cd61f51026bfda0b0085c
Ariana <152944814+aam214@users.noreply.github.com>
2024-03-30 11:32:54 -0500

    Added bootstrap spinner to loading.

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -49,5 +49,7 @@ if (loaded){
 
 } else {
   load();
-  return "Loading";
+  return <div class="spinner-border text-primary" role="status">
+  <span class="visually-hidden">Loading...</span>
+</div>
 }}
