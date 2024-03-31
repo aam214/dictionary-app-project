d64d3147bc78b0aa2b6940cedfcb887932a13c49
Ariana <152944814+aam214@users.noreply.github.com>
2024-03-31 16:55:46 -0500

    Added link to photo and hover css.

--- src/Photos.js
+++ src/Photos.js
@@ -7,7 +7,9 @@ export default function Photos(props){
   <section>
     <div className="Photos">
     {props.photos.map(function (photo, index) {
-      return <img src={photo.src.small} alt="Search Images" className="img-fluid" key={index} />;
+      return <a href={photo.src.original} target="_blank" title="Original Images">
+      <img src={photo.src.small} alt="Search Images" className="img-fluid" key={index} />
+      </a>;
   })}
   </div> 
  </section>
